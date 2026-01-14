99f9cfdecf30469b35713afae79a6b17797a2246
sahil8303 <sahilkawadse2003@gmail.com>
2026-01-15 01:06:00 +0530

    Added Joi validation and error handling

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const app=express();
 const mongoose=require("mongoose")
 const Listing =require("./models/listing.js")
 const path = require("path")
-const ejsMate= require("ejs-mate")
+const ejsMate= require("ejs-mate");
+const ExpressError = require("./utils/Expresserror.js");
+const {listingSchema} = require('./Schema.js');
+
+
+const wrapAsync = require("./utils/wrapAsync.js");
+
+
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust"
 
 main()
@@ -29,8 +36,21 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,"/public")))
 
+//Schema Validation using JOI NPM PACKAGE
+//WE HAVE USED THIS FUNCTION AS MIDDLE WARE
+const validateListing=(req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let erMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,erMsg);
+    }
+    else{
+        next();
+    }
+}
+
 //INDEX ROUTE
-app.get("/listings",async (req,res)=>{
+app.get("/listings",wrapAsync (async (req,res)=>{
     const allListings = await Listing.find({});
 //     console.log(
 //   allListings.map(l => ({
@@ -39,26 +59,43 @@ app.get("/listings",async (req,res)=>{
 //   }))
 // );
     res.render("listings/index.ejs",{allListings});
-})
+}))
 //NEW ROUTE
 app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs")
 })
 
 //SHOW ROUTE
-app.get("/listings/:id",async (req,res)=>{
+app.get("/listings/:id", wrapAsync (async (req,res)=>{
     let {id}=req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
-})
+}))
 
 //CREATE ROUTE
-app.post("/listings", async (req,res)=>{
-    // let {title,description,image,price,country,location} = req.body;
-    let newListing = new Listing(req.body.listing);
+//we are using try and catch to ensure error hadling it was bulky thats why we have shift to wrapAsync
+// app.post("/listings", async (req,res,next)=>{
+//     try{
+//     const newListing = new Listing(req.body.listing);
+//     await newListing.save();
+//     res.redirect("/listings")
+//     }
+
+//     catch(err){
+//         next(err)
+//     }
+    
+// })
+app.post("/listings",validateListing, wrapAsync (async (req,res,next)=>{
+    // if(!req.body.listing){
+    //     throw new ExpressError(400,"Send valid data for listening")
+    // }
+    
+    const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings")
-})
+  
+}));
 
 //EDIT ROUTE
 app.get("/listings/:id/edit",async (req,res)=>{
@@ -68,21 +105,21 @@ app.get("/listings/:id/edit",async (req,res)=>{
 
 });
 //update route
-app.put("/listings/:id", async (req,res)=>{
+app.put("/listings/:id",validateListing, wrapAsync (async (req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, req.body.listing);
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //DELETE ROUTE
-app.delete("/listings/:id",async (req,res)=>{
+app.delete("/listings/:id", wrapAsync (async (req,res)=>{
     let {id}=req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings")
     
 
-})
+}));
 
 
 // app.get("/testlisting",async (req,res)=>{
@@ -103,6 +140,18 @@ app.get("/",(req,res)=>{
     res.send("Hi,I am root")
 })
 
+
+
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Page not found"));
+});
+
+//error handling middleware
+app.use((err,req,res,next)=>{
+    let {statusCode = 500, message = "Something went wrong"} = err;
+    
+    res.status(statusCode).render("error.ejs",{message});
+})
 app.listen(8080,()=>{
     console.log("Server is listening to port 8080");
     
